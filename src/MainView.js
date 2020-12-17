a7d7ece3c2eb7aa691bb665ef25712baf6484f7c
Benjamin Binder <benjamin.binder@kite-consult.de>
2020-12-17 12:26:19 +0100

    MainView: add advanced search
    - add search field for artist, release year and genre
    - add reset button which resets all fields
    - field are now controlled
    - add badges for artist, year and genre

--- src/MainView.js
+++ src/MainView.js
@@ -4,6 +4,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 import "./MainView.css";
 import ResultsArea from "./results/resultsArea";
@@ -14,6 +17,12 @@ const MainView = () => {
   const [loading, setLoading] = useState(true);
   const [albums, setAlbums] = useState(null);
   const [filteredAlbums, setFilteredAlbums] = useState(null);
+  const [advancedAlbums, setAdvancedAlbums] = useState(null);
+  const [searchFields, setSearchFields] = useState({
+    artist: "",
+    year: "",
+    genre: "",
+  });
 
   useEffect(() => {
     fetchAlbums()
@@ -28,16 +37,68 @@ const MainView = () => {
       .finally(setLoading(false));
   }, []);
 
-  const handleChange = e => {
+  const handleTitleChange = e => {
     e.preventDefault();
-    const filteredAlbums = albums.filter(album =>
-      album["im:name"].label
-        .toLowerCase()
-        .includes(e.target.value.toLowerCase())
-    );
+    const filterAlbums = albums =>
+      albums.filter(album =>
+        album["im:name"].label
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase())
+      );
+    let filteredAlbums = filterAlbums(advancedAlbums ? advancedAlbums : albums);
+
+    //reset filtered albums when search term is empty
+    if (e.target.value === "") {
+      filteredAlbums = null;
+    }
     setFilteredAlbums(filteredAlbums);
   };
 
+  const handleAdvancedSearchChange = e => {
+    const fieldId = e.target.id;
+    setSearchFields({ ...searchFields, [fieldId]: e.target.value });
+  };
+
+  const submitAdvancedSearch = () => {
+    //start either with albums or filtered albums
+    let startAlbums = filteredAlbums ? filteredAlbums : albums;
+
+    if (searchFields.artist) {
+      startAlbums = startAlbums.filter(album =>
+        album["im:artist"].label
+          .toLowerCase()
+          .includes(searchFields.artist.toLowerCase())
+      );
+      setAdvancedAlbums(startAlbums);
+    }
+
+    if (searchFields.year) {
+      startAlbums = startAlbums.filter(album =>
+        album["im:releaseDate"].attributes.label
+          .toLowerCase()
+          .includes(searchFields.year.toLowerCase())
+      );
+
+      setAdvancedAlbums(startAlbums);
+    }
+
+    if (searchFields.genre) {
+      startAlbums = startAlbums.filter(album =>
+        album["category"].attributes.term
+          .toLowerCase()
+          .includes(searchFields.genre.toLowerCase())
+      );
+
+      setAdvancedAlbums(startAlbums);
+    }
+
+    //if all advanced filters are empty, set startAlbums back to filtered or unfiltered albums
+    if (!searchFields.artist && !searchFields.year && !searchFields.genre) {
+      setAdvancedAlbums(null);
+    }
+  };
+
+  // conditoinal rendering for the albums
   let results = null;
   if (loading) {
     results = (
@@ -50,7 +111,17 @@ const MainView = () => {
       </Row>
     );
   } else if (albums) {
-    results = <ResultsArea albums={filteredAlbums ? filteredAlbums : albums} />;
+    results = (
+      <ResultsArea
+        albums={
+          advancedAlbums
+            ? advancedAlbums
+            : filteredAlbums
+            ? filteredAlbums
+            : albums
+        }
+      />
+    );
   }
 
   return (
@@ -64,14 +135,99 @@ const MainView = () => {
           </Col>
         </Row>
         <Row className="d-flex justify-content-start align-items-start">
-          <Form as={Col} xs={8} md={6} lg={4}>
-            <Form.Group controlId="search-group">
-              <Form.Control
-                placeholder="Search for Title ..."
-                onChange={handleChange}
-              ></Form.Control>
-            </Form.Group>
-          </Form>
+          <Accordion as={Col} xs={8} md={6} className="accordion">
+            <Card>
+              <Card.Header>
+                <Form onSubmit={e => e.preventDefault()}>
+                  <Form.Group controlId="title">
+                    <Form.Control
+                      placeholder="Search for title"
+                      onChange={handleTitleChange}
+                    />
+                  </Form.Group>
+                </Form>
+                <Row>
+                  <Col>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey="0"
+                      className="link-button"
+                    >
+                      Advanced Search
+                    </Accordion.Toggle>
+                  </Col>
+                </Row>
+                <Row className="d-flex justify-content-start align-items-center">
+                  <Col>
+                    <Button bsPrefix="button reset-button">Reset</Button>
+
+                    {Object.values(searchFields).map((filter, idx) => {
+                      if (filter) {
+                        return (
+                          <div className="badge" key={idx}>
+                            {filter}
+                          </div>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </Col>
+                </Row>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Form onSubmit={e => e.preventDefault()}>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="artist">
+                          <Form.Control
+                            placeholder="Artist"
+                            onChange={handleAdvancedSearchChange}
+                          />
+                          <Form.Text className="text-muted">
+                            Search for artist
+                          </Form.Text>
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Form.Group lg={6} as={Col} controlId="year">
+                        <Form.Control
+                          placeholder="Realease Year"
+                          onChange={handleAdvancedSearchChange}
+                        />
+                        <Form.Text className="text-muted">
+                          Search for release year
+                        </Form.Text>
+                      </Form.Group>
+
+                      <Form.Group lg={6} as={Col} controlId="genre">
+                        <Form.Control
+                          placeholder="Genre"
+                          onChange={handleAdvancedSearchChange}
+                        />
+                        <Form.Text className="text-muted">
+                          Search for genre
+                        </Form.Text>
+                      </Form.Group>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Button
+                          bsPrefix="button"
+                          onClick={submitAdvancedSearch}
+                        >
+                          Apply
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         </Row>
         {results}
       </Container>
