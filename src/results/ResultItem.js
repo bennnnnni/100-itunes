b2ec0ae12d1f4c5ba1ca2fbc9201c540cacbc361
Benjamin Binder <benjamin.binder@kite-consult.de>
2020-12-28 14:04:28 +0100

    ResultItem: display year and genre

--- src/results/ResultItem.js
+++ src/results/ResultItem.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import "./ResultItem.scss";
 
-const ResultItem = ({ result, idx }) => {
+const ResultItem = ({ result }) => {
+  console.log(result);
   return (
     <div className="result-card">
       <div className="card-header">
         <div className="result-rank">
           <p>{result.rank}</p>
         </div>
+        <p className="card-header-content">{result.category.attributes.term}</p>
+        <p className="card-header-content">
+          {result["im:releaseDate"].attributes.label.split(" ")[2]}
+        </p>
       </div>
       <img src={result["im:image"][2].label} alt="" />
       <p className="result-title">{result["im:name"].label}</p>
