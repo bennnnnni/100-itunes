2b9cb7a4a85402c755b9183919aba92240c02825
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-11 13:09:50 +0100

    ResultItem: make fav. checkbox only available for logged in users

--- src/results/ResultItem.js
+++ src/results/ResultItem.js
@@ -3,10 +3,12 @@ import React from "react";
 import Checkbox from "@material-ui/core/Checkbox";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./ResultItem.scss";
 
 const ResultItem = ({ result }) => {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="result-card">
       <div className="card-header">
@@ -18,13 +20,15 @@ const ResultItem = ({ result }) => {
       </div>
       <div className="img-container">
         <img src={result.img} alt="" />
-        <Checkbox
-          className="img-checkbox"
-          color="secondary"
-          size="medium"
-          icon={<FavoriteBorderIcon color="secondary" fontSize="large" />}
-          checkedIcon={<FavoriteIcon color="secondary" fontSize="large" />}
-        />
+        {isAuthenticated && (
+          <Checkbox
+            className="img-checkbox"
+            color="secondary"
+            size="medium"
+            icon={<FavoriteBorderIcon color="secondary" fontSize="large" />}
+            checkedIcon={<FavoriteIcon color="secondary" fontSize="large" />}
+          />
+        )}
       </div>
       <p className="result-title">{result.name}</p>
       <p className="result-artist">{result.artist}</p>
