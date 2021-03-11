cffd28ad093238ec1cc54d67d1bca8a3e0de77b1
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-11 10:39:33 +0100

    ResultItem: add Icon checkbox for favorization

--- src/results/ResultItem.js
+++ src/results/ResultItem.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Checkbox from "@material-ui/core/Checkbox";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
 import "./ResultItem.scss";
 
 const ResultItem = ({ result }) => {
@@ -12,7 +16,16 @@ const ResultItem = ({ result }) => {
         <p className="card-header-content">{result.genre}</p>
         <p className="card-header-content">{result.year}</p>
       </div>
-      <img src={result.img} alt="" />
+      <div className="img-container">
+        <img src={result.img} alt="" />
+        <Checkbox
+          className="img-checkbox"
+          color="secondary"
+          size="medium"
+          icon={<FavoriteBorderIcon color="secondary" fontSize="large" />}
+          checkedIcon={<FavoriteIcon color="secondary" fontSize="large" />}
+        />
+      </div>
       <p className="result-title">{result.name}</p>
       <p className="result-artist">{result.artist}</p>
     </div>
