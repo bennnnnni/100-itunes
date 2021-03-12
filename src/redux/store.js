25f80e92245916f8eb9ab09a0bead9dec776d773
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-12 11:54:22 +0100

    store: add redux-thunk as middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const composedEnhancer = composeWithDevTools();
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 const store = createStore(rootReducer, composedEnhancer);
 
