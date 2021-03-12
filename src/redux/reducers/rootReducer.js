c129b2a2fa634768e2a06bbf2b52067aaafb2c25
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-12 11:58:18 +0100

    rootReducer: add podcastReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import { albumReducer } from "./albumSlice";
+import { podcastReducer } from "./podcastSlice";
 
 const rootReducer = combineReducers({
   albums: albumReducer,
+  podcasts: podcastReducer,
 });
 
 export default rootReducer;
