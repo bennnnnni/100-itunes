6244accc4c36f12cd82f0394d84ff936a6149634
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-12 11:55:03 +0100

    albumSlice: add thunk function to fetch albums into store

--- src/redux/reducers/albumSlice.js
+++ src/redux/reducers/albumSlice.js
@@ -1,4 +1,13 @@
 import { SET_ALBUMS } from "../actions";
+import { fetchAlbums } from "../../api";
+import { setAlbums } from "../actionCreators";
+import { transformAlbums } from "../../utils";
+
+export const fetchAlbumsIntoStore = async (dispatch, getState) => {
+  const response = await fetchAlbums();
+  const transformedAlbums = transformAlbums(response);
+  dispatch(setAlbums(transformedAlbums));
+};
 
 export const albumReducer = (state = [], action) => {
   switch (action.type) {
