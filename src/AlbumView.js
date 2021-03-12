17e81146900b36c624ad589bd0301d8c704d334f
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-12 11:56:01 +0100

    AlbumView: uses redux store

--- src/AlbumView.js
+++ src/AlbumView.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
 import MainView from "./MainView";
-import { fetchAlbums } from "./api";
-import { transformAlbums } from "./utils";
+import { fetchAlbumsIntoStore } from "./redux/reducers/albumSlice";
 
 const AlbumView = () => {
-  const [albums, setAlbums] = useState(null);
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const albums = useSelector(state => state.albums);
 
   useEffect(() => {
-    fetchAlbums()
-      .then(res => {
-        const transformedAlbums = transformAlbums(res);
-        setAlbums(transformedAlbums);
-        setLoading(false);
-      })
-      .catch(e => {
-        console.error(e);
-        setError(e);
-      });
+    try {
+      dispatch(fetchAlbumsIntoStore);
+      setLoading(false);
+    } catch (e) {
+      console.error(e);
+      setError(e);
+      setLoading(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  console.log("albums keck", albums);
   return <MainView items={albums} error={error} loading={loading} />;
 };
 
