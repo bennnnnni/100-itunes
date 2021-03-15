f12cf026cb3a00f6dee333776157a8fd729b7154
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-15 17:35:44 +0100

    actionCreators: add loading and error cases

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,4 +1,11 @@
-import { SET_ALBUMS, SET_PODCASTS } from "./actions";
+import {
+  SET_ALBUMS,
+  SET_PODCASTS,
+  SET_ALBUM_ERROR,
+  SET_ALBUM_LOADING,
+  SET_PODCAST_ERROR,
+  SET_PODCAST_LOADING,
+} from "./actions";
 
 export const setAlbums = payload => {
   return {
@@ -10,6 +17,34 @@ export const setAlbums = payload => {
 export const setPodcasts = payload => {
   return {
     type: SET_PODCASTS,
+    payload: payload,
+  };
+};
+
+export const setPodcastError = payload => {
+  return {
+    type: SET_PODCAST_ERROR,
+    payload: payload,
+  };
+};
+
+export const setPodcastLoading = payload => {
+  return {
+    type: SET_PODCAST_LOADING,
+    payload: payload,
+  };
+};
+
+export const setAlbumError = payload => {
+  return {
+    type: SET_ALBUM_ERROR,
+    payload: payload,
+  };
+};
+
+export const setAlbumLoading = payload => {
+  return {
+    type: SET_ALBUM_LOADING,
     payload: payload,
   };
 };
