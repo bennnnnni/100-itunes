82ace1d0ca2ad503abb3ddcb77ff190ed77cae42
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-15 17:36:30 +0100

    albumSlice: add loading and error cases

--- src/redux/reducers/albumSlice.js
+++ src/redux/reducers/albumSlice.js
@@ -1,18 +1,33 @@
-import { SET_ALBUMS } from "../actions";
+import { SET_ALBUMS, SET_ALBUM_ERROR, SET_ALBUM_LOADING } from "../actions";
 import { fetchAlbums } from "../../api";
-import { setAlbums } from "../actionCreators";
+import { setAlbumError, setAlbumLoading, setAlbums } from "../actionCreators";
 import { transformAlbums } from "../../utils";
 
+const initialState = { items: null, loading: true, error: "" };
+
 export const fetchAlbumsIntoStore = async (dispatch, getState) => {
-  const response = await fetchAlbums();
-  const transformedAlbums = transformAlbums(response);
-  dispatch(setAlbums(transformedAlbums));
+  try {
+    const response = await fetchAlbums();
+    const transformedAlbums = transformAlbums(response);
+    dispatch(setAlbums(transformedAlbums));
+    dispatch(setAlbumLoading(false));
+  } catch (e) {
+    dispatch(setAlbumError(e));
+    dispatch(setAlbumLoading(false));
+    console.error(e);
+  }
 };
 
-export const albumReducer = (state = [], action) => {
+export const albumReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_ALBUMS: {
-      return [...action.payload];
+      return { ...state, items: [...action.payload] };
+    }
+    case SET_ALBUM_ERROR: {
+      return { ...state, error: action.payload };
+    }
+    case SET_ALBUM_LOADING: {
+      return { ...state, loading: action.payload };
     }
     default:
       return state;
