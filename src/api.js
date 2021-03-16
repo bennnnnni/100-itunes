4ebf52f302e848dbaf4402957e488387cb6ffba4
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-16 14:39:27 +0100

    api: fix proxy issues

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 export const fetchPodcasts = async () => {
-  const proxyURL = "https://thingproxy.freeboard.io/fetch/";
+  const proxyURL = "https://api.allorigins.win/get?url=";
   const url =
     "https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/100/explicit.json";
-  const response = await fetch(proxyURL + url);
+  const response = await fetch(proxyURL + encodeURIComponent(url));
+  console.log(response);
   const data = await response.json();
-  return data.feed.results;
+  const parsedData = await JSON.parse(data.contents);
+  return parsedData.feed.results;
 };
 
 export const fetchAlbums = async () => {
