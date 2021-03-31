a3260f5d533af7b32d4b3d8c6f5c0ace903ab027
Benjamin Binder <benjamin.binder@kite-consult.de>
2021-03-31 10:45:35 +0200

    App: WIP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import AlbumView from "./AlbumView";
 import Navigation from "./Navbar/Navbar";
 import PodcastView from "./PodcastView";
 import FavoritView from "./FavoritView";
 import { appViews } from "./constants";
+import { getUser } from "./api";
 
 const { ALBUMS, PODCASTS, FAVORITES } = appViews;
 
 function App() {
   const currentView = useSelector(state => state.appState.currentView);
+  const { isAuthenticated, user } = useAuth0();
+  useEffect(() => {
+    const getUserOrCreateNewUser = async () => {
+      const data = await getUser("46");
+      if (data.user) {
+        // update all the favorites
+        console.log("user exists");
+      } else {
+        // craete new user
+        console.log("user doesnt exist");
+      }
+    };
+    if (isAuthenticated) {
+      getUserOrCreateNewUser();
+    }
+  });
 
   const renderCorrectView = () => {
     if (currentView === ALBUMS) {
